7e5e063e5f0a8fdcfbd3c9f5ceb194708d7ff46d
Jillian Wysoki <jillianwysoki@gmail.com>
2018-08-01 18:50:07 -0500

    rectangle

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,24 +1,24 @@
-package shapes;
-
-public class Square extends Rectangle {
-
-    public Square(double side){
-        super()
-    }
-
-    public Square(double length, double width){
-        this.length = length;
-        this.width = width;
-
-    }
-
-    @Override
-    public double getArea() {
-        return super.getArea();
-    }
-
-    @Override
-    public double getPerimeter() {
-        return super.getPerimeter();
-    }
-}
+//package shapes;
+//
+//public class Square extends Rectangle {
+//
+//    public Square(double side){
+//        super()
+//    }
+//
+//    public Square(double length, double width){
+//        this.length = length;
+//        this.width = width;
+//
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return super.getArea();
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        return super.getPerimeter();
+//    }
+//}
